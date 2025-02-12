243aa6f6de5c475f1bcb9aa8f323af8d68267f11
nikita75rus <166195472+nikita75rus@users.noreply.github.com>
2025-02-12 14:10:48 +0900

    Add files via upload

--- PythonHW11.py
+++ PythonHW11.py
@@ -8,4 +8,8 @@ from marvel import full_dict
 
 # Преобразование словаря в список словарей
 список_фильмов: List[Dict[str, Any]] = [{"id": k, **v} for k, v in full_dict.items()]
-
+# Добавляем функции фильтрации и работы с множествами
+отфильтрованные_фильмы: List[Dict[str, Any]] = list(filter(lambda x: x["id"] in числа, список_фильмов))
+режиссеры: Set[str] = {фильм["director"] for фильм in список_фильмов}
+фильмы_год_строка: Dict[str, Dict[str, Any]] = {k: {**v, "year": str(v["year"])} for k, v in full_dict.items()}
+фильмы_на_ч: List[Dict[str, Any]] = list(filter(lambda x: x["title"] and x["title"].startswith("Ч"), список_фильмов))
